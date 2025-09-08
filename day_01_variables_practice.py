50332394905d0d09a7a4ac15a2f211892fe0bc2a
Ashwin M Shetty <work.ashwinshetty@gmail.com>
2025-09-08 19:23:33 +0530

    Add files via upload

---
+++ day_01_variables_practice.py
@@ -0,0 +1,22 @@
+# Numbers
+age = 25
+height = 5.8
+temperature = -10
+
+# Strings
+name = "Ashwin Shetty"
+profession = "Mechanical Engineer"
+goal = "AI/ML Engineer"
+
+# Booleans
+is_learning = True
+has_experience = False
+
+# Lists (collections)
+skills = ["Python", "Problem Solving", "Mathematics"]
+subjects = ["Statistics", "Linear Algebra", "Machine Learning"]
+
+# Printing all elements with their types
+print(f"Name: {name} (Type: {type(name)})")
+print(f"Age: {age} (Type: {type(age)})")
+print(f"Skills: {skills} (Type: {type(skills)})")
