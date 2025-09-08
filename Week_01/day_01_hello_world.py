ec3aba60cc9b7e19bd6d96d5c2b5577e47b28a8c
Ashwin M Shetty <work.ashwinshetty@gmail.com>
2025-09-08 10:00:00 +0530

    Day 1: First Python program - Hello World and basic syntax

---
+++ Week_01/day_01_hello_world.py
@@ -0,0 +1,26 @@
+# Day 1 : My AIML Journey begins
+# Ashwin Shetty - Mechanical Engineer -> AIML Engineer
+
+print("Hello, AI/ML World!")  
+print("Today is my first day of 90 days into AIML Journey.")
+print("From mechanical to neural networks")
+
+name = "Ashwin"
+background = "Mechanical Engineer"
+goal = "AIML Engineer"
+target = "90 days of learning"
+
+print(f"My name is '{name} .")
+print(f"My background is in {background}.")
+print(f"My goal is to become an {goal}.")
+print(f"My target is to complete {target}.")
+
+days_total = 90
+days_completed = 1
+days_remaining = days_total - days_completed
+
+print(f"I have completed {days_completed} day out of {days_total}.")
+print(f"I have {days_remaining} days remaining to achieve my goal.")
+print(f"Completion Percentage: {(days_completed/days_total)*100}%")
+
+print("Lets build the future with AI/ML!")
